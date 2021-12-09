08fae003e4f103f813ee29a2955468ca5818b2aa
elenasheshukova <elenasheshukova@gmail.com>
2021-12-09 21:12:51 +0300

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Елена Червоткина on 09.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
